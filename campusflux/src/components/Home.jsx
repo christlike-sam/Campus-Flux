5e83694789e6cd686809818c07d7dc5c35fcbfff
christlike <christlikesam@gmail.com>
2024-07-03 07:31:15 +0300

    modified:   campusflux/src/components/Home.jsx 	modified:   campusflux/src/css/Home.css

--- campusflux/src/components/Home.jsx
+++ campusflux/src/components/Home.jsx
@@ -6,11 +6,10 @@ export default function Home(){
         <div >
             <div className="home">
                 {/* <img src="https://images.pexels.com/photos/827528/pexels-photo-827528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img> */}
-                <h1 className="home-text">WHY CHOOSE US?</h1>
-                <ul className="abt-text">Flexibility - You work at your convenience from any place using your device.<br /> 
-                Reliability - We are the top remote job company in Africa. <br />
-                Quick Payouts - You can quickly and convenientyly withdraw to your M-Pesa,
-                once your payments are ready.</ul>
+                <h1 className="home-text">START EARNING NOW!</h1>
+                <ul className="abt-text">We are flexible - You work at your convenience from any place using your device.<br /> 
+                Quick Payouts - Quickly and conveniently withdraw profits to your M-Pesa.<br />
+                Reliability - We are the top remote job company in Africa.</ul>
                 </div>
                 </div>
     </div>
