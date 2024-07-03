3faf63d8271f6cd0cb46024015925465605923fe
christlike <christlikesam@gmail.com>
2024-07-03 19:00:19 +0300

    new file:   Mockup Designs/Home.png 	new file:   Mockup Designs/Home.psd 	modified:   campusflux/src/components/Home.jsx 	modified:   campusflux/src/css/Home.css

--- campusflux/src/components/Home.jsx
+++ campusflux/src/components/Home.jsx
@@ -3,14 +3,16 @@ export default function Home(){
 
   return (
     <div>
-        <div >
-            <div className="home">
+                    <div className="plans">
                 {/* <img src="https://images.pexels.com/photos/827528/pexels-photo-827528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img> */}
-                <h1 className="home-text">START EARNING NOW!</h1>
+                {/* <h1 className="home-text">START EARNING NOW!</h1>
                 <ul className="abt-text">We are flexible - You work at your convenience from any place using your device.<br /> 
                 Quick Payouts - Quickly and conveniently withdraw profits to your M-Pesa.<br />
-                Reliability - We are the top remote job company in Africa.</ul>
+                Reliability - We are the top remote job company in Africa.</ul> */}
+                <div className="freeplan">
+                  Free Plan
                 </div>
+                <div className="premiumplan"> Premium Plan</div>
                 </div>
     </div>
   )
