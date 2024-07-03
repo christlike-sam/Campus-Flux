dfc92139d8f272437228eb0358a553451014b19f
christlike <christlikesam@gmail.com>
2024-07-03 06:39:19 +0300

    modified:   campusflux/src/components/Navbar.jsx 	modified:   campusflux/src/css/Header.css

--- campusflux/src/components/Navbar.jsx
+++ campusflux/src/components/Navbar.jsx
@@ -10,6 +10,8 @@ export default function Navbar(){
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i></a>
 </ul>
       </div>
     </nav>
