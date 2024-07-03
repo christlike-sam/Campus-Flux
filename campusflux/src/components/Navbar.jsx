ed8e859093c9b967dbb6ee4a292545f61f8c9668
christlike <christlikesam@gmail.com>
2024-07-03 19:33:40 +0300

    modified:   campusflux/src/components/Navbar.jsx 	modified:   campusflux/src/css/Header.css

--- campusflux/src/components/Navbar.jsx
+++ campusflux/src/components/Navbar.jsx
@@ -1,19 +1,26 @@
-import { useState, useRef } from 'react';
-import "../css/Header.css"
-export default function Navbar(){
+import { useState } from 'react';
+import "../css/Header.css";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav>
       <div className='nav'>
-      <ul>
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i></a>
-</ul>
+        <ul className={isOpen ? 'open' : ''}>
+          <a href="#home">Home</a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+          <div className="icon" onClick={toggleMenu}>
+            <i className="fa fa-bars"></i>
+          </div>
+        </ul>
       </div>
     </nav>
   );
-};
+}
